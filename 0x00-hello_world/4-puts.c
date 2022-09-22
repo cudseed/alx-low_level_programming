59aee794161596feb511375c53d7519eb46c3c54
cudseed <cud.badger@gmail.com>
2022-09-22 03:08:17 -0700

    corrections

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+
+* main - prints the phrase "Programming is like building a multilingual puzzle
+
+* Return: 0 if existed properly, non-zero otherwise
+
+*/
+
+int main(void)
+
+{
+
+ puts("\"Programming is like building a multilingual puzzle");
+
+ Return(0)
+
+}
