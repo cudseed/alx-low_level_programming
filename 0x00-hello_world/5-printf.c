770620fe4dd2b5905d053544b86ed768e518cdba
cudseed <cud.badger@gmail.com>
2022-09-22 06:37:57 -0700

    corrections

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+/**
+
+* main - prints the phrase withe proper grammar,
+
+* but the outcome is a piece of art,
+
+* Return: 0 if existed properly, non-zero otherwise
+
+*/
+
+int main(void)
+
+{
+
+printf("with proper grammar, but the outcome is a piece of
+
+art,
+");
+
+return (0);
+
+}
