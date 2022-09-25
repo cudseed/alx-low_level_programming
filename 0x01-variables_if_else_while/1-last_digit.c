3715b29b4ad8037676abeeb432a0ebf0ed05e133
cudseed <cud.badger@gmail.com>
2022-09-25 06:33:46 -0700

    corrections

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,12 +12,12 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%d is positive", n);
-else if (n < 0)
-printf("%d is negative", n);
-else
-printf("%d is zero", n);
-printf("\n");
+printf("Last digit of %d is %d", n, n % 10);
+if (n % 10 > 5)
+printf("and is greater than 5\n");
+else if (n % 10 == 0)
+printf("and is 0\n");
+else if (n % 10 < 6 && n % 10 != 0)
+printf("and is less than 6 and not 0\n");
 return (0);
 }
