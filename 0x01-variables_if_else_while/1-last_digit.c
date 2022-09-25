451f95b1f6148c151e4165408c44f000e029d6d0
cudseed <cud.badger@gmail.com>
2022-09-25 06:03:36 -0700

    corrections

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - program execution starts here
+ * Description: this is the main function
+ * Return: an integer to signify success or fail
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive", n);
+else if (n < 0)
+printf("%d is negative", n);
+else
+printf("%d is zero", n);
+printf("\n");
+return (0);
+}
