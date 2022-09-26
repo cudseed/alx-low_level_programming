66abb045bd5f1ce9d373e85ba1403d930d48e50d
cudseed <cud.badger@gmail.com>
2022-09-25 19:22:40 -0700

    corrections

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print "putchar" followed by a new line
+ * Description: You are not allowed to include standard librairies.
+ * Return: 0
+ */
+int main(void)
+{
+char putchar[] = "_putchar";
+int i = 0;
+while (putchar[i] != '\0')
+{
+_putchar(putchar[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
+
+
